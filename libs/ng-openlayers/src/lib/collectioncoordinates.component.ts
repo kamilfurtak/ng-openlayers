71515823977ccdc774d8a2205541b73255abe106
Kamil Furtak <kamil.furtak@gmail.com>
2024-07-27 18:44:41 +0200

    Format file

--- libs/ng-openlayers/src/lib/collectioncoordinates.component.ts
+++ libs/ng-openlayers/src/lib/collectioncoordinates.component.ts
@@ -9,9 +9,9 @@ import { Coordinate } from 'ol/coordinate';
 import { transform } from 'ol/proj';
 
 @Component({
-    selector: 'aol-collection-coordinates',
-    template: ` <div class="aol-collection-coordinates"></div> `,
-    standalone: true,
+  selector: 'aol-collection-coordinates',
+  template: ` <div class="aol-collection-coordinates"></div> `,
+  standalone: true,
 })
 export class CollectionCoordinatesComponent implements OnChanges, OnInit {
   @Input()
