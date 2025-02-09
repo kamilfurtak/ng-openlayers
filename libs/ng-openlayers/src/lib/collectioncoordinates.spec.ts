7d4a5b09578e87e61ab09cac29df1a35b40a976b
Kamil Furtak <kamil@fcomp.dev>
2025-02-09 17:19:32 +0100

    Add tests for initializing with various geometry hosts
    Enhanced the test suite to validate `CollectionCoordinatesComponent` initialization with different geometry host components. This ensures compatibility and correctness across all supported geometry types. Also improved spy setup for better test clarity.

--- libs/ng-openlayers/src/lib/collectioncoordinates.spec.ts
+++ libs/ng-openlayers/src/lib/collectioncoordinates.spec.ts
@@ -1,6 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { CollectionCoordinatesComponent } from './collectioncoordinates.component';
-import { GeometryLinestringComponent, MapComponent } from '../public-api';
+import {
+  GeometryLinestringComponent,
+  GeometryMultiLinestringComponent,
+  GeometryMultiPointComponent,
+  GeometryMultiPolygonComponent,
+  GeometryPolygonComponent,
+  MapComponent,
+} from '../public-api';
 
 describe('CollectionCoordinatesComponent', () => {
   let component: CollectionCoordinatesComponent;
@@ -10,7 +17,7 @@ describe('CollectionCoordinatesComponent', () => {
   beforeEach(() => {
     mockMapComponent = {
       instance: {
-        on: jasmine.createSpy(),
+        on: jasmine.createSpy('on'),
         getView: jasmine.createSpy().and.returnValue({
           getProjection: jasmine
             .createSpy()
@@ -38,8 +45,34 @@ describe('CollectionCoordinatesComponent', () => {
   });
 
   describe('constructor', () => {
-    it('should initialize component with the correct host', () => {
-      expect(component['host']).toBe(mockHostComponent);
+    it('should initialize with a GeometryLineStringComponent host', () => {
+      const geometryHostMock = new GeometryLinestringComponent(mockHostComponent, {} as any);
+      component = new CollectionCoordinatesComponent(mockHostComponent, geometryHostMock, null, null, null, null);
+      expect(component['host']).toBe(geometryHostMock);
+    });
+
+    it('should initialize with a GeometryPolygonComponent host', () => {
+      const geometryHostMock = new GeometryPolygonComponent(mockHostComponent, {} as any);
+      component = new CollectionCoordinatesComponent(mockHostComponent, null, geometryHostMock, null, null, null);
+      expect(component['host']).toBe(geometryHostMock);
+    });
+
+    it('should initialize with a GeometryMultiPointComponent host', () => {
+      const geometryHostMock = new GeometryMultiPointComponent(mockHostComponent, {} as any);
+      component = new CollectionCoordinatesComponent(mockHostComponent, null, null, geometryHostMock, null, null);
+      expect(component['host']).toBe(geometryHostMock);
+    });
+
+    it('should initialize with a GeometryMultiLinestringComponent host', () => {
+      const geometryHostMock = new GeometryMultiLinestringComponent(mockHostComponent, {} as any);
+      component = new CollectionCoordinatesComponent(mockHostComponent, null, null, null, geometryHostMock, null);
+      expect(component['host']).toBe(geometryHostMock);
+    });
+
+    it('should initialize with a GeometryMultiPolygonComponent host', () => {
+      const geometryHostMock = new GeometryMultiPolygonComponent(mockHostComponent, {} as any);
+      component = new CollectionCoordinatesComponent(mockHostComponent, null, null, null, null, geometryHostMock);
+      expect(component['host']).toBe(geometryHostMock);
     });
 
     it('should throw an error if no host geometry component is provided', () => {
@@ -99,13 +132,6 @@ describe('CollectionCoordinatesComponent', () => {
   });
 
   describe('transformCoordinates', () => {
-    // beforeEach(() => {
-    //   component.coordinates = [
-    //     [0, 0],
-    //     [1, 1],
-    //   ];
-    // });
-
     it('should not transform coordinates if srid matches mapSrid', () => {
       component.coordinates = [
         [0, 0],
